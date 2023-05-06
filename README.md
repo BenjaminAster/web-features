67d60516e462a904b73e7b53e2863395db41fa21
Benjamin Aster <nimajneb0905@gmail.com>
2023-05-06 13:16:07 +0200

    1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 
+# Web Features
+
+An overview of many web features that are not yet fully supported in all three major browser engines. These range from features already implemented in two browser engines up to unofficial drafts and even just proposals.
+
+[benjaminaster.com/web-features](https://benjaminaster.com/web-features/)
+
+<!-- 
 - https://drafts.fxtf.org
 - https://drafts.csswg.org
 - https://wicg.io
@@ -12,3 +19,4 @@
 - https://open-ui.org
 - https://github.com/orgs/webmachinelearning/repositories
 - https://github.com/orgs/immersive-web/repositories
+ -->
--- main.deno.ts
+++ main.deno.ts
@@ -6,7 +6,7 @@ deno run --allow-read --allow-write=. main.deno.ts
 */
 
 // @ts-ignore
-import { DOMParser as _DOMParser } from "https://deno.land/x/deno_dom@v0.1.31-alpha/deno-dom-wasm.ts";
+import { DOMParser as _DOMParser } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts";
 const DOMParser: typeof globalThis.DOMParser = _DOMParser;
 
 import features from "./features.ts";
@@ -14,16 +14,16 @@ import features from "./features.ts";
 const document = new DOMParser().parseFromString(await Deno.readTextFile("./template.html"), "text/html");
 document
 
-const template = document.querySelector<HTMLTemplateElement>("template#item");
-const engineTemplate = document.querySelector<HTMLTemplateElement>("template#engine");
+const template = document.querySelector("template#item");
+const engineTemplate = document.querySelector("template#engine");
 
 for (const [category, featureList] of Object.entries(features)) {
 	for (const [feature, support, url] of featureList as any) {
-		const clone = (template.cloneNode(true) as HTMLTemplateElement).content;
+		const clone = template.cloneNode(true).content;
 		clone.querySelector(".name").textContent = feature;
 		clone.querySelector("a").setAttribute("href", url);
 		for (const { engine, engineName, labels } of support) {
-			const engineClone = (engineTemplate.cloneNode(true) as HTMLTemplateElement).content;
+			const engineClone = engineTemplate.cloneNode(true).content;
 			engineClone.querySelector(".engine").classList.add(engine, ...labels);
 			engineClone.querySelector(".engine").setAttribute("title", `Supported in ${engineName}${labels.length ? ` (${labels.join(", ")})` : ""}`);
 			clone.querySelector(".engines").append(engineClone);
